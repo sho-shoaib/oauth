6651f998c5aca2408f0cc87c509c36c9620ebaac
Shoaib Shaikh <shaikhsho292@gmail.com>
2022-02-26 13:41:02 +0530

    home updated

--- src/components/HomeWorksGridPer.js
+++ src/components/HomeWorksGridPer.js
@@ -1,12 +1,38 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Typography } from "@mui/material";
+import React from "react";
 
-const HomeWorksGridPer = ({children}) => {
+const HomeWorksGridPer = ({ children, trans, mr, ml, color, bg }) => {
   return (
-    <Box sx={{p: 5, border: 10, display: "inline-block", pt: 15, pb: 15, borderRadius:5, transform: "translateY(-100px)"}}>
-        <Typography variant='h1' sx={{fontWeight: 500, fontSize: 35, fontWeight: 400, color: "rgba(245, 230, 83, 0.9)"}}>{children}</Typography>
+    <Box
+      sx={{
+        p: 3,
+        border: 10,
+        display: "inline-block",
+        pt: 15,
+        pb: 35,
+        borderRadius: 5,
+        transform: `translateY(-${trans}px)`,
+        mr: `${mr}`,
+        ml: `${ml}`,
+        borderColor: bg || "white",
+        minWidth: 250,
+      }}
+    >
+      <Typography
+        variant='h1'
+        component='h2'
+        sx={{
+          fontWeight: 500,
+          fontSize: { xs: 30, md: 32 },
+          fontWeight: 400,
+          color: color || "#f5f5f5",
+          textAlign: "center",
+        }}
+      >
+        {children}
+      </Typography>
     </Box>
-  )
-}
+  );
+};
 
-export default HomeWorksGridPer
+export default HomeWorksGridPer;
--- src/sections/HomeServices.js
+++ src/sections/HomeServices.js
@@ -1,67 +1,104 @@
-import { Grid, Paper, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import HomeWorksGridPer from "./HomeWorksGridPer";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import React from "react";
+import HomeWorksGridPer from "../components/HomeWorksGridPer";
 
-const HomeWorksGrid = () => {
+const HomeServices = () => {
   return (
-    <Box sx={{ width: "100%" }}>
+    <Container
+      maxWidth='xl'
+      sx={{
+        backgroundColor: "#f5f5f5",
+        color: "black",
+        mt: "2vh",
+        overflow: "hidden",
+        maxHeight: { xs: 900, md: 730 },
+        mb: "2vh",
+        pt: 3,
+      }}
+    >
+      <Box
+        sx={{
+          maxWidth: "lg",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: { xs: "flex-start", sm: "center" },
+          marginInline: "auto",
+          flexDirection: { xs: "column", sm: "row" },
+          gap: { xs: 2, sm: 0 },
+        }}
+      >
+        <Typography
+          sx={{
+            borderBottom: 5,
+            borderColor: "#ffdf00",
+            fontSize: { xs: 30, sm: 32, lg: 35 },
+            fontWeight: 500,
+            color: "black",
+          }}
+        >
+          Services we offer
+        </Typography>
+      </Box>
+
       <Box
         sx={{
           width: "100%",
           display: "flex",
-          justifyContent: "center",
           flexDirection: "column",
           alignItems: "center",
         }}
       >
         <Box
           sx={{
-            p: 5,
             border: 10,
+            borderColor: "#6F4686",
             display: { xs: "none", md: "grid" },
-            pt: 10,
-            pb: 10,
-            borderRadius: 5,
-            transform: "translateY(-100px)",
-            mb: 10,
-            minWidth: 250,
             placeItems: "center",
+            borderRadius: "50%",
+            height: 300,
+            width: 300,
+            transform: "translateY(-140px)",
+            mb: 10,
           }}
         >
           <Typography
-            variant='h1'
+            variant='h5'
+            component='h2'
             sx={{
-              fontWeight: 500,
-              fontSize: { xs: 30, md: 35 },
-              fontWeight: 400,
-              color: "rgba(245, 230, 83, 0.9)",
+              fontSize: 35,
+              textAlign: "center",
+              transform: "translateY(30px)",
             }}
           >
-            Unlisted
+            Managing <br />
+            portfolio
           </Typography>
         </Box>
-        <Box
-          sx={{
-            transform: "translateY(60px)",
-            display: { xs: "none", md: "flex" },
-          }}
-        >
-          <HomeWorksGridPer trans='0' mr='-40px'>
-            Commodities
+
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          <HomeWorksGridPer color='black' bg='#6F4686' trans='0' mr='-40px'>
+            Ipo <br />
+            Account
           </HomeWorksGridPer>
-          <HomeWorksGridPer trans='80' mr='-40px'>
-            Derivative
+          <HomeWorksGridPer color='black' bg='#6F4686' trans='80' mr='-40px'>
+            Trading <br />
+            Account
           </HomeWorksGridPer>
-          <HomeWorksGridPer trans='150'>Equity</HomeWorksGridPer>
-          <HomeWorksGridPer trans='80' ml='-40px'>
-            Currency
+          <HomeWorksGridPer color='black' bg='#6F4686' trans='150'>
+            Demat <br />
+            Account
           </HomeWorksGridPer>
-          <HomeWorksGridPer trans='0' ml='-40px'>
-            Mutual fund
+          <HomeWorksGridPer color='black' bg='#6F4686' trans='80' ml='-40px'>
+            Margin <br />
+            Account
+          </HomeWorksGridPer>
+          <HomeWorksGridPer color='black' bg='#6F4686' trans='0' ml='-40px'>
+            Stocks <br />
+            Advisory
           </HomeWorksGridPer>
         </Box>
-        <Grid container spacing={2} sx={{ width: "100%", mt: 4.5, mb: 10 }}>
+
+        <Grid container spacing={2} sx={{ width: "100%", mt: 5, mb: 10 }}>
           <Grid item xs={12}>
             <Box
               sx={{
@@ -70,6 +107,7 @@ const HomeWorksGrid = () => {
                 border: 10,
                 borderRadius: 5,
                 textAlign: "center",
+                borderColor: "#6F4686",
               }}
             >
               <Typography
@@ -78,10 +116,10 @@ const HomeWorksGrid = () => {
                   fontWeight: 500,
                   fontSize: { xs: 30, md: 35 },
                   fontWeight: 400,
-                  color: "rgba(245, 230, 83, 0.9)",
+                  color: "black",
                 }}
               >
-                Unlisted
+                Managing portfolio
               </Typography>
             </Box>
           </Grid>
@@ -94,6 +132,7 @@ const HomeWorksGrid = () => {
                 border: 10,
                 borderRadius: 5,
                 textAlign: "center",
+                borderColor: "#6F4686",
               }}
             >
               <Typography
@@ -102,10 +141,11 @@ const HomeWorksGrid = () => {
                   fontWeight: 500,
                   fontSize: { xs: 30, md: 35 },
                   fontWeight: 400,
-                  color: "rgba(245, 230, 83, 0.9)",
+                  color: "black",
                 }}
               >
-                Unlisted
+                Ipo <br />
+                Account
               </Typography>
             </Box>
           </Grid>
@@ -118,6 +158,7 @@ const HomeWorksGrid = () => {
                 border: 10,
                 borderRadius: 5,
                 textAlign: "center",
+                borderColor: "#6F4686",
               }}
             >
               <Typography
@@ -126,10 +167,11 @@ const HomeWorksGrid = () => {
                   fontWeight: 500,
                   fontSize: { xs: 30, md: 35 },
                   fontWeight: 400,
-                  color: "rgba(245, 230, 83, 0.9)",
+                  color: "black",
                 }}
               >
-                Unlisted
+                Demat <br />
+                Account
               </Typography>
             </Box>
           </Grid>
@@ -142,6 +184,7 @@ const HomeWorksGrid = () => {
                 border: 10,
                 borderRadius: 5,
                 textAlign: "center",
+                borderColor: "#6F4686",
               }}
             >
               <Typography
@@ -150,10 +193,11 @@ const HomeWorksGrid = () => {
                   fontWeight: 500,
                   fontSize: { xs: 30, md: 35 },
                   fontWeight: 400,
-                  color: "rgba(245, 230, 83, 0.9)",
+                  color: "black",
                 }}
               >
-                Unlisted
+                Margin <br />
+                Account
               </Typography>
             </Box>
           </Grid>
@@ -166,6 +210,7 @@ const HomeWorksGrid = () => {
                 border: 10,
                 borderRadius: 5,
                 textAlign: "center",
+                borderColor: "#6F4686",
               }}
             >
               <Typography
@@ -174,10 +219,11 @@ const HomeWorksGrid = () => {
                   fontWeight: 500,
                   fontSize: { xs: 30, md: 35 },
                   fontWeight: 400,
-                  color: "rgba(245, 230, 83, 0.9)",
+                  color: "black",
                 }}
               >
-                Unlisted
+                Stocks <br />
+                Advisory
               </Typography>
             </Box>
           </Grid>
@@ -190,6 +236,7 @@ const HomeWorksGrid = () => {
                 border: 10,
                 borderRadius: 5,
                 textAlign: "center",
+                borderColor: "#6F4686",
               }}
             >
               <Typography
@@ -198,17 +245,17 @@ const HomeWorksGrid = () => {
                   fontWeight: 500,
                   fontSize: { xs: 30, md: 35 },
                   fontWeight: 400,
-                  color: "rgba(245, 230, 83, 0.9)",
+                  color: "black",
                 }}
               >
-                Unlisted
+                Trading Account
               </Typography>
             </Box>
           </Grid>
         </Grid>
       </Box>
-    </Box>
+    </Container>
   );
 };
 
-export default HomeWorksGrid;
+export default HomeServices;
